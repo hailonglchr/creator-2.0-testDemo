b7462bb6b5746573c6341e9e2a1a1f3c51a9c7c4
hailonglchr <15011292320@163.com>
2018-08-17 11:35:27 +0800

    first commit

---
+++ assets/scripts/testJS.js
@@ -0,0 +1,53 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+        mainPanel: cc.Sprite,
+        testLabel: cc.Label,
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        // this.testLabel.string = 10 + "%";
+        var self = this;
+        cc.loader.loadResDir("Img", function(completedCount, totalCount, item){
+            var _progress = parseInt(completedCount / totalCount  * 100);
+            console.log("***Res load Progress >>> ", _progress + "%");
+            self.testLabel.string = _progress + "%";
+        },function (err, objects, urls) { 
+            console.log("***Res load Success...", objects);
+        });
+    },
+
+    // start () {
+
+    // },
+
+    // update (dt) {},
+});
